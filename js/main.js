bc0e99140445ac1f8a375601aa9e4e08dee06980
JaneIllario <illariojane@gmail.com>
2018-04-01 03:30:17 -0400

    run as soon as loaded
    linked main.js and overwrote 3s time with auto run upon load; timer there and changed to 1.5 sec (can switch back to it)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	
 	setTimeout(function(){
 		$("body").addClass('loaded');
-	}, 3000);
+	}, 1500);
 	
+});
+
+//<!--runs as soon as loaded-->
+$(function() {
+  $('#body').addClass('loaded');
 });
